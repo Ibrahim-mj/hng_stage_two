55970bca137dda2dfff4cb18d07c288fe98db258
Ibrahim Abdulmojeed <ibraheemabdulmojeed1@gmail.com>
2023-09-13 20:40:30 +0100

    re-write the serializer validation logic

--- api/serializers.py
+++ api/serializers.py
@@ -6,33 +6,31 @@ from bleach import clean
 from .models import Person
 
 class PersonSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(max_length=100)
-    age = serializers.IntegerField()
+    name = serializers.CharField(
+        max_length=100, 
+        validators=[
+            validators.RegexValidator(r'^[a-zA-Z ]+$', 'Name must be letters only'),
+            UniqueValidator(queryset=Person.objects.all(), message='Name already exists, make it a bit different')
+        ]
+    )
+    age = serializers.IntegerField(
+        validators=[
+            validators.MinValueValidator(1, message='Age can not be less than 1'),
+            validators.MaxValueValidator(150, message='Age can not be greater than 150')
+        ]
+    )
     email = serializers.EmailField(
         validators=[
-            validators.EmailValidator(message="Provide a valid Email"), 
+            validators.EmailValidator(),
             UniqueValidator(queryset=Person.objects.all(), message="Email already exists")
         ]
     )
-    occupation = serializers.CharField(max_length=100)
-
-    def validate_name(self, value):
-        if not value.isalpha():
-            raise serializers.ValidationError('Name must be letters only')
-        return clean(value)
-
-    def validate_age(self, value):
-        if not value >= 1:
-            raise serializers.ValidationError('Age can not be less than 1')
-        if not isinstance(value, int):
-            raise serializers.ValidationError('Age must be a number')
-        return clean(value)
-    
-    def validate_occupation(self, value):
-        if not value.isalpha():
-            raise serializers.ValidationError('Occupation must be letters only')
-        return clean(value)
-    
+    occupation = serializers.CharField(
+        max_length=100, 
+        validators=[
+            validators.RegexValidator(r'^[a-zA-Z ]+$', 'Occupation must be letters only')
+        ]
+    )
     class Meta:
         model = Person
         fields = ['name', 'age', 'email', 'occupation']
